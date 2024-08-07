13d98b07bec9835baf2a8024982ebc141cc12606
viclito <berglin1998@gmail.com>
2024-08-07 14:53:13 +0530

    third initialization

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-      // '/api':'http://guidestarindia.momsuat.com'
-    }
-  },
+  // server:{
+  //   proxy:{
+  //     '/api':'http://guidestarindia.momsuat.com'
+  //   }
+  // },
   plugins: [react()],
 })
